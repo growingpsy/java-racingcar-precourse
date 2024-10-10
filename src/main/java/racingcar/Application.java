9ff19246da0b4a85adb099875cbe730406fe8921
Si Yeong Park <psy915@swu.ac.kr>
2024-10-10 14:36:08 +0900

    feat: 문자열이 숫자인지 확인하는 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,4 +1,5 @@
 package racingcar;
+
 import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -3,8 +3,19 @@ package utils;
 public class GameUtils {
     public GameUtils() {
     }
-    public boolean isRightCarName(String userCarsName){
-        if(userCarsName.length() <=5) return true;
-        return false;
+
+    // 자동차 이름의 길이가 5자 이하인지 확인하는 메서드
+    public boolean isRightCarName(String userCarsName) {
+        return userCarsName.length() <= 5;
+    }
+
+    // 입력된 문자열이 숫자인지 확인하는 메서드
+    public boolean isNumber(String userNumber) {
+        for (int i = 0; i < userNumber.length(); i++) {
+            if (!Character.isDigit(userNumber.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
